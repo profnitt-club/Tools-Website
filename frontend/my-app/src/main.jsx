c04ba5b7e41f08a2d63a45d3d1a2f53d7296adfe
Divyansh <divyanshkumaragrawal80@gmail.com>
2025-02-17 23:56:20 +0530

    public folder chnage

--- frontend/my-app/src/main.jsx
+++ frontend/my-app/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* Wrap your App with BrowserRouter and set basename */}
+    <BrowserRouter basename="/Tools-Website">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
